58cc2d26aec62fab5d9848db2d1f289d47db1a5d
techykani <kani.karthi2k20@gmail.com>
2025-02-07 09:49:53 +0530

    commit

--- AccountService/src/main/java/com/example/demo/controller/AccountController.java
+++ AccountService/src/main/java/com/example/demo/controller/AccountController.java
@@ -23,6 +23,7 @@ public class AccountController {
 
 	@PostMapping("/create")
 	public String saveAccount(@RequestBody Account account) {
+		System.out.println("saved");
 		return service.createAccount(account);
 	}
 
